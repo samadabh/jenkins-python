53f652b5a1b24cadaa5dce72c819b62fdd2c8c5e
samadabh <156426915+samadabh@users.noreply.github.com>
2024-02-14 10:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,30 @@ from email.message import EmailMessage
 useremail = "saaketh9616@gmail.com"
 password = "Cristiano@cr7"
 
-url = "ise.com/partner"
+
+def send_email(body):
+
+    email_sender = 'saaketh9616@gmail.com'
+    email_password = 'ubglzrjydzemnhgt'
+    email_receiver = 'howf163@gmail.com'
+    subject = 'Status of ISE Partner Portal'
+    # body = """
+    
+    # """
+
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+        # Log in and send the email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
 def check_ise_partner_portal_status():
     service = Service()
     options = webdriver.ChromeOptions()
@@ -26,42 +49,53 @@ def check_ise_partner_portal_status():
     time.sleep(5)
     element2 = driver.find_element(By.ID,"okta-signin-submit")
     driver.execute_script("arguments[0].click();", element2)
-    time.sleep(5)
-    def element_exists():
+    time.sleep(30)
+
+    def feed_server_header_exists():
         try:
             driver.find_element(By.CSS_SELECTOR,"div.regHeader15")
         except NoSuchElementException:
             return False
         return True
 
-    
-    if element_exists:
+    if feed_server_header_exists:
         print("Partner Portal is working")
+        driver.get("https://ise.cisco.com/partner/#pageId=com_cisco_fsm_download_offline_update_page")
+        time.sleep(20)
+
+        def generate_button_exists():
+            try:
+                driver.find_element(By.ID,"generateBtn")
+            except NoSuchElementException:
+                return False
+            return True
+
+        if generate_button_exists:
+            driver.find_element(By.ID,"generateBtn").click()
+            time.sleep(10)
+        else:
+            print("Generate Package not avaiialbe")
+            send_email("Generate Package is not working")
+
+        def download_button_exists():
+            try:
+                driver.find_element(By.ID,"downloadBtn")
+            except NoSuchElementException:
+                return False
+            return True
+
+        # if download_button_exists:
+        #     # driver.find_element(By.ID,"downloadBtn").click()
+        #     time.sleep(10)
+        if not download_button_exists:
+            print("Download Package not avaiialbe")
+        
+        while True:
+            pass
     else:
         print("Partner Portal is not working")
+        send_email("Partner Portal is not working")
 
-        email_sender = 'saaketh9616@gmail.com'
-        email_password = 'ubglzrjydzemnhgt'
-        email_receiver = 'howf163@gmail.com'
-
-
-        subject = 'Status of ISE Partner Portal'
-        body = """
-        Partner Portal is not working
-        """
-
-        em = EmailMessage()
-        em['From'] = email_sender
-        em['To'] = email_receiver
-        em['Subject'] = subject
-        em.set_content(body)
-
-        # Add SSL (layer of security)
-        context = ssl.create_default_context()
-
-        # Log in and send the email
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(email_sender, email_password)
-            smtp.sendmail(email_sender, email_receiver, em.as_string())
+        
 
 check_ise_partner_portal_status()
