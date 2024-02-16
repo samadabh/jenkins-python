a21069129019fea69a7201b6ed93370cadbf7ec0
samadabh <156426915+samadabh@users.noreply.github.com>
2024-02-16 11:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def send_email(body,screenshot):
 
     context = ssl.create_default_context()
 
-    with smtplib.SMTP_SSL('smtp.gmail.com', 587, timeout=120) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout=120) as smtp:
         smtp.login(email_sender, email_password)
         smtp.sendmail(email_sender, email_receiver, em.as_string())
         smtp.sendmail(email_sender, email_receiver2, em.as_string())
